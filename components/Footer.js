5813c9f8579686dec2ce7c4bc4a0f5120a1cc4bf
Bhavy Rastogi <bhavyrastogi2002@gmail.com>
2021-11-22 21:30:19 +0530

    quick fixes 4

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from '../styles/footer.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <h1 className={styles.logo}>Recource Hub</h1>
+            <h1 className={styles.logo}>Resource Hub</h1>
             <h3 className={styles.foot_text}>Made with <span style={{color:"#E02401"}}>❤</span> by one of you</h3>
         </div>
     )
