18911043327b84735b846b70f854ac13e3c82ee8
Bhavy Rastogi <bhavyrastogi2002@gmail.com>
2022-01-11 23:30:44 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
-
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+  <>
+  <Script
+    id="1"
+    strategy='lazyOnload' 
+    src={`https://www.googletagmanager.com/gtag/js?id=G-F309KB24PZ`}
+  />
+
+  <Script
+    id="2"
+    strategy='lazyOnload'>
+      { `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-F309KB24PZ');`}
+    </Script> 
+   <Component {...pageProps} />
+   </>
+  );
 }
 
 export default MyApp
